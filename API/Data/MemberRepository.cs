221773c73c2be1583e88321834943045b6e3cdd9
“Prathamesh <prathamesh.more9405@gmail.com>
2026-03-04 14:08:05 +0530

    End of section 10

--- API/Data/MemberRepository.cs
+++ API/Data/MemberRepository.cs
@@ -1,4 +1,3 @@
-using System;
 using API.Entities;
 using API.Interfaces;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +9,13 @@ public class MemberRepository(AppDbContext context) : IMemberRepository
     public async Task<Member?> GetMemberByIdAsync(string id)
     {
         return await context.Members.FindAsync(id);
+    }
+
+    public async Task<Member?> GetMemberForUpdate(string id)
+    {
+        return await context.Members
+            .Include(x => x.User)
+            .SingleOrDefaultAsync(x => x.Id == id);
     }
 
     public async Task<IReadOnlyList<Member>> GetMembersAsync()
